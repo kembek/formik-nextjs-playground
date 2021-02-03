0b2a7aabb5e83594fe0275b46d1f39b757465148
kembek <zevs.312.777@gmail.com>
2021-02-04 00:31:08 +0600

    feat: table was created

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,24 @@
 import Head from 'next/head';
-import { TODO } from '../components';
 
+import { TODO } from '../components';
+import { CONTENT_ID, Main } from '../layout';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="container">
       <Head>
         <title>Home page</title>
       </Head>
 
-      <main id="content" className={styles.main}>
+      <Main className={styles.main}>
         <h1 className={styles.title}>Home page</h1>
         <p>
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam mollitia quia laboriosam dicta accusamus culpa tenetur illo
           officia excepturi. Autem mollitia asperiores voluptatem fuga eum, iusto molestiae? Beatae, doloremque? Asperiores.
         </p>
         <TODO text="Add a content" />
-      </main>
+      </Main>
     </div>
   );
 }
--- pages/table-page/index.tsx
+++ pages/table-page/index.tsx
@@ -1,16 +1,42 @@
+import { Main } from '../../layout';
+
 export default function TablePage() {
   return (
-    <section>
-      <h1>Table page</h1>
-      <p
-        style={{
-          fontSize: 36,
-          textTransform: 'uppercase',
-          color: 'red',
-        }}
-      >
-        TODO
-      </p>
-    </section>
+    <div>
+      <Main>
+        <table>
+          <caption>Things to eat</caption>
+          <thead>
+            <tr>
+              <th scope="col">Product</th>
+              <th scope="col">Price</th>
+              <th scope="col">Vegan</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">Peach</th>
+              <td>1.99 €</td>
+              <td>yes</td>
+            </tr>
+            <tr>
+              <th scope="row">Cucumber</th>
+              <td>0.99 €</td>
+              <td>yes</td>
+            </tr>
+            <tr>
+              <th scope="row">Cheeseburger</th>
+              <td>7.99 €</td>
+              <td>no</td>
+            </tr>
+            <tr>
+              <th scope="row">Eggplant</th>
+              <td>1.35 €</td>
+              <td>yes</td>
+            </tr>
+          </tbody>
+        </table>
+      </Main>
+    </div>
   );
 }
